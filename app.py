198707203fa6b463fb5e5ee8a052b7be13dd8251
pranay rewane <rewanepranay@gmail.com>
2021-03-11 20:18:38 +0530

    changes

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import networkx as nx
 import regex
 from flask import Flask, request, jsonify, render_template
 import nltk
+import detect_features
+import pickle
 # nltk.download('stopwords')
 
 def read_article(data):
@@ -86,6 +88,24 @@ def generate_summary(file_name, top_n=5):
 #----------FLASK-----------------------------#
 
 app = Flask(__name__)
+model = pickle.load(open('decisiontree_pickle_model','rb'))
+@app.route('/predict',methods = ['POST'])
+def predict():
+    url = request.form['url']
+    res=detect_features.generate_data_set(url)
+    res = np.array(res).reshape(1,-1)
+    pred= model.predict(res)
+    
+    isphishing=pred[0]
+    if isphishing==1:
+        prediction="not a phishing site"
+    else:
+        prediction="a phishing site"
+    
+    print(prediction)
+
+    #output = round(prediction[0], 2)
+    return render_template('index1.html', prediction_text=prediction)
 @app.route('/templates', methods =['POST'])
 def original_text_form():
 		text = request.form['input_text']
