4c3af5fcc6d72c84fd1bdb1ba7376058f8652eb1
PranayRewane <49708468+PranayRewane@users.noreply.github.com>
2021-03-11 22:13:50 +0530

    Update detect_features.py

--- detect_features.py
+++ detect_features.py
@@ -4,7 +4,8 @@ import urllib.request
 from bs4 import BeautifulSoup
 import socket
 import requests
-from googlesearch import search
+from google import search
+# from googlesearch import search
 import whois
 from datetime import datetime
 import time
